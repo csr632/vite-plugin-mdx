e8fc2673447113f01ed9745e062dd9de93045553
csr632 <632882184@qq.com>
2020-05-26 18:37:50 +0800

    chore: add mdx plugin example

---
+++ examples/plugin/vite.config.ts
@@ -0,0 +1,18 @@
+import type { UserConfig } from 'vite'
+import { createPlugin } from 'vite-plugin-mdx'
+import react from 'vite-plugin-react'
+import remarkToc from 'remark-toc'
+import remarkSlug from 'remark-slug'
+
+module.exports = {
+  jsx: 'react',
+  plugins: [
+    react,
+    createPlugin({
+      remarkPlugins: [remarkToc, remarkSlug]
+    })
+  ],
+  rollupInputOptions: {
+    // external: ['@pika/react', '@pika/react-dom']
+  }
+} as UserConfig
